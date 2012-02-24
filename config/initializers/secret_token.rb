1f58ac4469c03cbdf4647ca90bc73948d9273984
Samuel Esposito <samuel.esposito@gmail.com>
2012-02-24 11:33:43 +0100

    initial rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nomad::Application.config.secret_token = 'e130ac5cf1cef31ef4d983a9765668945a8a01db1448ef8dcdeba1c5254db0b34b21edf8d7ad797d2caa09fed5c82031843d84239ef3a5f9d2e78ba5b95fa916'
