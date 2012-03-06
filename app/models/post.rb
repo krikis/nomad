6d273bade39a7fd561a13877af67ecb8a1babdcf
Samuel Esposito <samuel.esposito@gmail.com>
2012-03-06 20:59:53 +0100

    add sanitize method to model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+
+  before_validation :sanitize_attributes
+
+  private
+
+  def sanitize_attributes
+    attributes.each do |key, value|
+      if value.is_a? String
+        self.send("#{key}=", Sanitize.clean(value))
+      end
+    end
+  end
+
 end
