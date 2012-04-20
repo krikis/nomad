b603f3a44728111743e64c7b4939261c21d187a5
Samuel Esposito <samuel.esposito@gmail.com>
2012-04-20 11:53:24 +0200

    add answers acceptance test

---
+++ spec/acceptance/answers_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+feature 'list answers' do
+
+  before { visit '/answers' }
+
+  scenario 'shows a list of answers' do
+    page.should have_css('div#answers')
+  end
+
+  scenario 'provides a new answer button', :js => true do
+
+  end
+
+end
