7ede78baee358ced2b06607b65f7a92afb96207d
Samuel Esposito <samuel.esposito@gmail.com>
2012-05-15 13:04:30 +0200

    write out pid to pidfile

--- faye/server.rb
+++ faye/server.rb
@@ -1,11 +1,17 @@
 require 'rubygems'
 
+ROOT_DIR = File.expand_path(File.dirname(__FILE__))
+File.open(File.join(ROOT_DIR, '..', 'tmp', 'pids', 'faye.pid'), 'w') do |f|
+  f << Process.pid
+end
+
 port   = ARGV[0] || 9292
 secure = ARGV[1] == 'ssl'
 shared = File.expand_path('../../shared', __FILE__)
 
 require File.expand_path('../app', __FILE__)
 Faye::WebSocket.load_adapter('thin')
+
 # Faye::Logging.log_level = :debug
 
 EM.run {
