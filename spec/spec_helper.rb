748d213a72ad3c355300ca29f972e25552a36e61
Samuel Esposito <samuel.esposito@gmail.com>
2012-05-19 20:40:23 +0200

    start faye test server with spork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,9 @@ Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However,
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
+  # Start faye server
+  require_relative 'support/faye_server_helper'
+  # Require rails
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
