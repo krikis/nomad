698b36d2b5d1c99b21acfb4e7094b444aa61c196
Samuel Esposito <samuel.esposito@gmail.com>
2012-06-15 10:42:16 +0200

    colorize publish output

--- faye/app.rb
+++ faye/app.rb
@@ -18,6 +18,36 @@ App.bind(:disconnect) do |client_id|
   # error "[ DISCONNECT] #{client_id}"
 end
 
+def light_blue_on_black
+  "\e[40m\e[1;36m"
+end
+
+def green_on_black
+  "\e[40m\e[1;32m"
+end
+
+def reset
+  "\e[0m"
+end
+
+def highlight_keyword(string, keyword)
+  string.gsub! /\"#{keyword}\"=>\"([^"]+)\"/,  "\"#{light_blue_on_black}#{keyword}#{reset}\"=>\"\\1\""
+  string.gsub! /\"#{keyword}\"=>\[([^\]]+)\]/, "\"#{light_blue_on_black}#{keyword}#{reset}\"=>[\\1]"
+  string.gsub! /\"#{keyword}\"=>\{([^\}]+)\}/, "\"#{light_blue_on_black}#{keyword}#{reset}\"=>{\\1}"
+end
+
+def highlight_string_attribute(string, attribute)
+  string.gsub! /\"#{attribute}\"=>\"([^"]+)\"/, "\"#{light_blue_on_black}#{attribute}#{reset}\"=>\"#{green_on_black}\\1#{reset}\""
+end
+
 App.bind(:publish) do |client_id, channel, data|
-  error "[    PUBLISH] #{client_id} #{channel} #{data}"
+  output = data.inspect
+  [
+    'new_versions', 'create', 'creates', 'versions', 'update', 'updates'
+  ].each{|keyword| highlight_keyword(output, keyword)}
+  [
+    'client_id'
+  ].each{|attribute| highlight_string_attribute(output, attribute)}
+  error output
+  # error "[    PUBLISH] #{client_id} #{channel} #{output}"
 end
