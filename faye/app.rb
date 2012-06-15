fcf1b3ceec83823880d18396af866a607085a40d
Samuel Esposito <samuel.esposito@gmail.com>
2012-06-15 10:53:52 +0200

    extend coloring schemes

--- faye/app.rb
+++ faye/app.rb
@@ -26,28 +26,52 @@ def green_on_black
   "\e[40m\e[1;32m"
 end
 
+def red_on_black
+  "\e[40m\e[1;31m"
+end
+
 def reset
   "\e[0m"
 end
 
-def highlight_keyword(string, keyword)
-  string.gsub! /\"#{keyword}\"=>\"([^"]+)\"/,  "\"#{light_blue_on_black}#{keyword}#{reset}\"=>\"\\1\""
-  string.gsub! /\"#{keyword}\"=>\[([^\]]+)\]/, "\"#{light_blue_on_black}#{keyword}#{reset}\"=>[\\1]"
-  string.gsub! /\"#{keyword}\"=>\{([^\}]+)\}/, "\"#{light_blue_on_black}#{keyword}#{reset}\"=>{\\1}"
+def highlight_keyword(string, keyword, color)
+  string.gsub! /\"#{keyword}\"=>\"([^"]+)\"/,  "\"#{color}#{keyword}#{reset}\"=>\"\\1\""
+  string.gsub! /\"#{keyword}\"=>\[([^\]]+)\]/, "\"#{color}#{keyword}#{reset}\"=>[\\1]"
+  string.gsub! /\"#{keyword}\"=>\{([^\}]+)\}/, "\"#{color}#{keyword}#{reset}\"=>{\\1}"
 end
 
-def highlight_string_attribute(string, attribute)
-  string.gsub! /\"#{attribute}\"=>\"([^"]+)\"/, "\"#{light_blue_on_black}#{attribute}#{reset}\"=>\"#{green_on_black}\\1#{reset}\""
+def highlight_string_attribute(string, attribute, color, content_color)
+  string.gsub! /\"#{attribute}\"=>\"([^"]+)\"/,
+               "\"#{color}#{attribute}#{reset}\"=>\"#{content_color}\\1#{reset}\""
+end
+
+def highlight_hash_attribute(string, attribute, color, content_color)
+  string.gsub! /\"#{attribute}\"=>\{([^\}]+)\}/,
+               "\"#{color}#{attribute}#{reset}\"=>#{content_color}{\\1}#{reset}"
+end
+
+def highlight_array_attribute(string, attribute, color, content_color)
+  string.gsub! /\"#{attribute}\"=>\[([^\]]+)\]/,
+               "\"#{color}#{attribute}#{reset}\"=>#{content_color}[\\1]#{reset}"
 end
 
 App.bind(:publish) do |client_id, channel, data|
   output = data.inspect
-  [
-    'new_versions', 'create', 'creates', 'versions', 'update', 'updates'
-  ].each{|keyword| highlight_keyword(output, keyword)}
-  [
-    'client_id'
-  ].each{|attribute| highlight_string_attribute(output, attribute)}
+  ['new_versions', 'create', 'creates',
+   'versions',     'update', 'updates'].each do |keyword|
+    highlight_keyword(output, keyword, light_blue_on_black)
+  end
+  ['version', 'remote_version'].each do |keyword|
+    highlight_hash_attribute(output,
+                             keyword,
+                             green_on_black,
+                             red_on_black)
+  end
+  ['client_id'].each do |attribute|
+    highlight_string_attribute(output,
+                               attribute,
+                               light_blue_on_black,
+                               green_on_black)
+  end
   error output
-  # error "[    PUBLISH] #{client_id} #{channel} #{output}"
 end
