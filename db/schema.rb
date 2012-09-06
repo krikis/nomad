057e0f31683c3625e7bffa9f6d67af1d5b5bdbf5
Samuel Esposito <s.esposito@roqua.nl>
2012-09-06 13:01:24 +0200

    cleanup schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120420085920) do
+ActiveRecord::Schema.define(:version => 20120224104216) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -21,13 +21,6 @@ ActiveRecord::Schema.define(:version => 20120420085920) do
     t.datetime "last_update"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
-  end
-
-  create_table "tasks", :force => true do |t|
-    t.string   "title"
-    t.string   "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
   end
 
 end
