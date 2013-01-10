7a282afe25a5da618112f4d21a4f442f140bf358
Samuel Esposito <samuel.esposito@gmail.com>
2013-01-10 11:47:14 +0100

    upgrade rails

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe AnswersController do
+
   describe "GET index" do
     before { get :index }
 
@@ -15,22 +16,6 @@ describe AnswersController do
 
   describe "GET new" do
     before { get :new }
-
-    it "renders the 'answers' template" do
-      response.should render_template("answers")
-    end
-  end
-
-  describe "GET show" do
-    before { get :show }
-
-    it "renders the 'answers' template" do
-      response.should render_template("answers")
-    end
-  end
-
-  describe "GET edit" do
-    before { get :edit }
 
     it "renders the 'answers' template" do
       response.should render_template("answers")
